27660557f351662f9b8823acf1e32c09e1fb66e0
PABlond <pierre-alexis.blond@live.fr>
2020-02-23 16:48:39 +0100

    update process

--- linbot.py
+++ linbot.py
@@ -5,6 +5,7 @@ import argparse
 import getpass
 import config
 import sys
+import requests
 
 class LinBot:
     def __init__(self, email: str, password: str, search: str):
@@ -15,6 +16,7 @@ class LinBot:
         self.button_connects = []
         self.search = search
         self.browser = None
+        self.store_connect = False
 
     def login(self):
         print('[] Login as {}'.format(self.email))
@@ -43,18 +45,60 @@ class LinBot:
         self.browser.goto(
             url=url_search.format(self.search))
         self.browser.scroll_bottom()
-        while True:
-            self.button_connects = self.browser.find_els_xpath(
-                xpath=config.selectors['connect'])
-            if len(self.button_connects) >= self.limit_connect:
-                break
-            self.search_page_n += self.limit_connect
-            self.browser.goto(url="{}&page={}".format(
-                url_search.format(self.search), str(self.search_page_n)))
+        time.sleep(1)
+
+        for x in self.browser.driver.find_elements_by_css_selector('.search-result__info a'):
+            # print('1', x.get_attribute("href"))
+            href = x.get_attribute("href")
+            if "https://www.linkedin.com/in/" in href:
+                profile_id = href.split("https://www.linkedin.com/in/")[1][:-1]
+                utils.append_ids(profile_id)
+
+        if self.store_connect is False:
+            self.connect()
+
+        max_pagination = self.browser.driver.find_elements_by_css_selector(
+            '.artdeco-pagination__indicator button')[-1].get_attribute(
+                'aria-label')
+        max_pagination = max_pagination.split(' ')[-1]
+        print(max_pagination)
+
+        if int(max_pagination) >= 2:
+            if int(max_pagination) > 2:
+                max_pagination = "2"
+            for y in range(2, int(max_pagination)):
+                self.search_page_n += 1
+                self.browser.goto(url="{}&page={}".format(
+                    url_search.format(self.search), self.search_page_n))
+                self.browser.scroll_bottom()
+                time.sleep(1)
+                if self.store_connect is False:
+                    self.connect()
+                for x in self.browser.driver.find_elements_by_css_selector('.search-result__info a'):
+                    # print('1', x.get_attribute("href"))
+                    href = x.get_attribute("href")
+                    if "https://www.linkedin.com/in/" in href:
+                        profile_id = href.split(
+                            "https://www.linkedin.com/in/")[1][:-1]
+                        utils.append_ids(profile_id)
+
+        # for x in self.browser.driver.find_elements_by_class_name("search-result__info"):
+        #     for y in x.find_elements_by_tag_name("a"):
+        #         print(y.get_attribute("href"))
+
+# while True:
+
+        #     if len(self.button_connects) >= self.limit_connect:
+        #         break
+        #     self.search_page_n += self.limit_connect
+        #     self.browser.goto(url="{}&page={}".format(
+        #         url_search.format(self.search), str(self.search_page_n)))
         print('\t-> Done\n')
 
     def connect(self):
         print('[] Connect with 2 people')
+        self.button_connects = self.browser.find_els_xpath(
+            xpath=config.selectors['connect'])[:1]
         for button_connect in self.button_connects[:self.limit_connect]:
             print(button_connect.get_attribute('aria-label'))
             button_connect.click()
@@ -62,24 +106,45 @@ class LinBot:
             button_send = self.browser.find_el_xpath(
                 config.selectors['connect_confirm'])
             button_send.click()
+            self.store_connect = True
             time.sleep(2)
+
+        for request in self.browser.driver.requests:
+            print("PATH", request.path)
+            print('HEADERS', request.headers)
+            if request.path == "https://www.linkedin.com/voyager/api/growth/normInvitations":
+                utils.store_request(
+                    headers=request.headers, body=request.body)
+
         print('\t-> Done\n')
 
     def close(self):
         print('[] Closing the browser')
         self.browser.driver.close()
-        print("[] Next round : {}".format(
-            utils.get_next_round(round_duration=300)))
+    
+    def send_reqs(self):
+        lines = utils.get_ids()
+        for id in lines:
+            print('[] Send request to {}'.format(id))
+            req = utils.set_request(profile_id=id)
+            r = requests.post("https://www.linkedin.com/voyager/api/growth/normInvitations", 
+                headers=req['headers'], data=req['body'])
+            print('\t-> Done')            
+            print("[] Next round : {}".format(
+                utils.get_next_round(round_duration=60)))
+            time.sleep(60)
+
 
     def run(self):
-        while True:
-            self.browser = headless.Browser()
-            utils.clear()
-            self.login()
-            self.search_page()
-            self.connect()
-            self.close()
-            time.sleep(300)
+        # while True:
+        self.browser = headless.Browser()
+        utils.clear()
+        self.login()
+        self.search_page()
+        # self.connect()
+        self.close()
+        self.send_reqs()
+        # time.sleep(300)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,9 +1,54 @@
 import datetime
+import json
 import os
+
 
 def get_next_round(round_duration: int):
     next_round = datetime.datetime.now() + datetime.timedelta(seconds=round_duration)
     return str(next_round.time()).split('.')[0]
 
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def get_ids():
+    with open('profiles_ids.lst', 'r') as f:
+        lines = [x.replace('\n', '') for x in f.readlines()]
+    return lines
+
+def append_ids(data: str):
+    if check_duplicate_ids(data) is True:
+        with open('profiles_ids.lst', 'a+') as f:
+            f.write("\n{}".format(data))
+
+def update_ids(profile_id: str):
+    lines = get_ids()
+    with open("profiles_ids.lst", "w") as f:
+        for line in lines:
+            if line.strip("\n") != profile_id:
+                f.write(line)
+
+
+
+def check_duplicate_ids(data: str):
+    lines = get_ids()
+    return not data in lines
+
+
+def store_request(headers, body):
+    with open('request.json', 'w') as f:
+        json.dump({
+            "headers": dict(headers),
+            "body": json.loads(body.decode())
+        },
+            f, ensure_ascii=False, indent=4)
+
+def get_request():
+    with open('request.json') as f:
+        data = json.load(f)
+    return data
+
+def set_request(profile_id: str):
+    req = get_request()
+    req['body']['invitee']['com.linkedin.voyager.growth.invitation.InviteeProfile']['profileId'] = profile_id
+    return req
